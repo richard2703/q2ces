d6c175744e034edcca6049ea8ae7553d8d61cd7e
Alejandra Ortiz <aortiz@sindinero.mx>
2022-09-27 12:46:13 -0500

    Alta de Accesorios Cambio

--- resources/views/equipos/altaDeAccesorios.blade.php
+++ resources/views/equipos/altaDeAccesorios.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main', ['activePage' => 'obra', 'titlePage' => __('Alta de Equipo')])
+@extends('layouts.main', ['activePage' => 'obra', 'titlePage' => __('Alta de Accesorios')])
 @section('content')
     <div class="content">
         <div class="container-fluid">
@@ -26,59 +26,15 @@
                                                 </div>
                                                                     
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Categoría:</label></br>
+                                                    <label class="labelTitulo">Año:</label></br>
                                                     <input type="text" class="inputCaja" id="" name="calle" value="">
                                                 </div>
                                                                     
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <div class="row align-items-end">
-                                                        <div class="col-8">
-                                                            <label class="labelTitulo">Tipo:</label></br>
-                                                             <select class="form-select" aria-label="Default select example">
-                                                                <option selected>Seleccione</option>
-                                                                    <option value="1">Maquinaria Pesada</option>
-                                                                    <option value="2">Equipo ligero</option>
-                                                                    <option value="1">Movimiento de Tierras</option>
-                                                            </select>
-                       
-                                                        </div>
-                                                        <div class="col-4">
-                                                            <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-plus-circle-fill btnMas" viewBox="0 0 16 16">
-                                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                                                            </svg>
-                                                        </div> 
-                                                    </div>   
-                                                </div>
-                                                                    
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Uso:</label></br>
-                                                    <input type="text" class="inputCaja" id="" name="calle" value="">
-                                                </div>
-                    
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
                                                     <label class="labelTitulo">Marca:</label></br>
-                                                        <input type="text" class="inputCaja" id="" name="calle" value="">
-                                                </div>
-                    
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Sub Marca:</label></br>
                                                     <input type="text" class="inputCaja" id="" name="calle" value="">
                                                 </div>
-                    
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Modelo:</label></br>
-                                                    <input type="text" class="inputCaja" id="" name="calle" value="">
-                                                </div>
-                    
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Motor:</label></br>
-                                                    <input type="text" class="inputCaja" id="" name="calle" value="">
-                                                </div>
-                    
-                                                <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Número Motor:</label></br>
-                                                    <input type="text" class="inputCaja" id="" name="calle" value="">
-                                                </div>
+
                     
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
                                                     <label class="labelTitulo">Número Serie:</label></br>
@@ -86,20 +42,24 @@
                                                 </div>
                     
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Número VIN:</label></br>
+                                                    <label class="labelTitulo">Modelo:</label></br>
                                                     <input type="text" class="inputCaja" id="" name="calle" value="">
                                                 </div>
 
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Obra Asignada:</label></br>
-                                                    <input type="text" class="inputCaja" id="" name="calle" value="">
+                                                <label class="labelTitulo">Maquinaria Asignada:</label></br>
+                                                    <select class="form-select" aria-label="Default select example">
+                                                        <option selected>Seleccione</option>
+                                                        <option value="1">Maquinaria 1</option>
+                                                        <option value="2">Maquinaria 2</option>
+                                                    </select>
                                                 </div>
 
                                                 <div class=" col-12 col-sm-6 col-lg-4 mb-3 ">
-                                                    <label class="labelTitulo">Operador Asignado:</label></br>
+                                                    <label class="labelTitulo">Color:</label></br>
                                                     <input type="text" class="inputCaja" id="" name="calle" value="">
                                                 </div>
-                                                
+
                                             </div>
                                         </div>
 
