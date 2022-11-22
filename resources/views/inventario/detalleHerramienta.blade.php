39b959f6be8f5f34e304b5dff9f96dcfbb0a536d
Alejandra Ortiz <aortiz@sindinero.mx>
2022-11-22 14:02:41 -0600

    Modal mover

--- resources/views/inventario/detalleHerramienta.blade.php
+++ resources/views/inventario/detalleHerramienta.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-11 align-self-center">
                         <div class="card-body contCart justify-content-center">
                     <div class="card ">
-                            <div class="p-1 align-self-start bacTituloPrincipal">
+                            <div class="card-header bacTituloPrincipal mb-5">
                                 <h2 class="my-3 ms-3 texticonos ">Detalle Herramienta</h2>
                             </div>
                             <form action="#" method="post"class="row alertaGuardar" enctype="multipart/form-data">
@@ -22,7 +22,9 @@
                                         <label for="mi-archivo">
                                             <span>sube imagen</span>
                                         </label>
+                                        
                                     </div>
+                                    
 
                                     <div class="col-12 text-center mb-3 ">
                                         <button type="submit" class="btn botonGral" onclick="alertaGuardar()">Guardar</button>
@@ -85,7 +87,7 @@
                                                 id="aler" name="aler" value="">
                                         </div>
 
-                                        <div class=" col-12 col-sm-6 col-lg-6 mb-3 ">
+                                        <div class=" col-12 col-sm-6 col-lg-6 mb-5 ">
                                             <label class="labelTitulo">Tipo:</label></br>
                                             <select class="form-select" aria-label="Default select example" id="sangre"
                                                 name="sangre">
@@ -97,8 +99,14 @@
 
                                             </select>
                                         </div>
-                                       
+                                        
+                                        <div class="col-12 text-center mt-5">
 
+                                            <button type="button" class="btn btn-primary botonGral" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                                Mover
+                                              </button>
+
+                                        </div>
                                     </div>
                                 </div>
                                 
@@ -174,13 +182,49 @@
          jQuery('span.'+idname).next().find('span').html(filename);
         });
 </script>
+<!-- Modal -->
 
-<style>
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header bacTituloPrincipal mb-3">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Mover</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form class="col-11">
+            <label class="labelTitulo">Tipo de movimiento:</label></br>
+            <select class="form-select" aria-label="Default select example" id=""  name="">
+                <option value="herramienta">Asignar</option>
+                <option value="refaccion">Desechar</option>
+            </select>
+
+            <label class="labelTitulo mt-3">Origen:</label></br>
+            <input type="text" class="inputCaja" id="aler" name="aler" value="">
+
+            <label class="labelTitulo mt-3">Destino:</label></br>
+            <input type="text" class="inputCaja" id="aler" name="aler" value="">
+
+            <label class="labelTitulo mt-3">Comentario:</label></br>
+            <textarea class="col-12 inputCaja mb-3"  ></textarea>
 
 
-   
+        </form>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary botonGral mx-auto">Mover</button>
+          </div>
+      </div>
+    </div>
+  </div>
 
-</style>
+
+
+
+
+
+
+
+
 
 
 @endsection
