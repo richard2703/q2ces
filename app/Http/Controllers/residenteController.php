91c92d4db82817cfe58680cd83c3b19002afa5a9
Alberto Ibarra <aibarra@suappi.com.mx>
2023-07-18 13:56:55 -0600

    Creación del soporte para residentes en otro bloque de datos.

---
+++ app/Http/Controllers/residenteController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\residente;
+use App\Models\obras;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+
+class residenteController extends Controller {
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+
+    public function index() {
+        // dd( 'Hola' );
+        abort_if ( Gate::denies( 'puesto_index' ), 403 );
+
+        $records = residente::select( 'residente.*',
+        DB::raw( 'obras.nombre AS obra' ) )
+        ->join( 'obras', 'obras.id', '=', 'residente.obraId' )
+        ->orderBy( 'nombre', 'asc' )->paginate( 10 );
+
+        $vctObras = obras::orderBy( 'nombre', 'asc' )->get();
+        // dd( $puestos );
+        return view( 'mtq.residentes', compact( 'records', 'vctObras' ) );
+    }
+
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+
+    public function create() {
+        //
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+
+    public function store( Request $request ) {
+        abort_if ( Gate::denies( 'catalogos_create' ), 403 );
+
+        // dd( $request );
+        $request->validate( [
+            'nombre' => 'required|max:250',
+            'comentario' => 'nullable|max:500',
+        ], [
+            'nombre.required' => 'El campo nombre es obligatorio.',
+            'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
+            'comentario.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
+        ] );
+        $record = $request->all();
+
+        residente::create( $record );
+        Session::flash( 'message', 1 );
+
+        return redirect()->route( 'residente.index' );
+    }
+
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+
+    public function show( $id ) {
+        //
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+
+    public function edit( $id ) {
+        //
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+
+    public function update( Request $request, $id ) {
+
+        abort_if ( Gate::denies( 'catalogos_edit' ), 403 );
+
+        // dd( $request );
+
+        $request->validate( [
+            'nombre' => 'required|max:250',
+            'comentario' => 'nullable|max:500',
+        ], [
+            'nombre.required' => 'El campo nombre es obligatorio.',
+            'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
+            'comentario.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
+        ] );
+        $data = $request->all();
+
+        $record = residente::where( 'id', $data[ 'controlId' ] )->first();
+
+        if ( is_null( $record ) == false ) {
+            // dd( $data );
+            $record->update( $data );
+            Session::flash( 'message', 1 );
+        }
+
+        return redirect()->route( 'residente.index' );
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+
+    public function destroy( $id ) {
+        //
+    }
+}
