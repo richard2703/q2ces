ad91144842fce405a3ece8e602422e0456717326
Kevin Bryan Jonathan Cobian Franco <kcobian@suappi.com.mx>
2023-08-24 12:21:24 -0600

    Ajustes en marca, y vista en su catalogo

--- app/Http/Controllers/marcaController.php
+++ app/Http/Controllers/marcaController.php
@@ -10,125 +10,136 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
-class marcaController extends Controller {
+class marcaController extends Controller
+{
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function index() {
+    public function index()
+    {
 
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = marca::orderBy( 'nombre', 'asc' )->paginate( 15 );
+        $records = marca::orderBy('nombre', 'asc')->paginate(15);
 
-        return view( 'catalogo.marcas', compact( 'records' ) );
+        return view('catalogo.marcas', compact('records'));
     }
 
     /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function create() {
+    public function create()
+    {
         //
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
 
-    public function store( Request $request ) {
+    public function store(Request $request)
+    {
 
-        abort_if ( Gate::denies( 'catalogos_create' ), 403 );
+        abort_if(Gate::denies('catalogos_create'), 403);
 
-        // dd( $request );
-        $request->validate( [
+        // dd($request);
+        $request->validate([
             'nombre' => 'required|max:250',
             'comentarios' => 'nullable|max:500',
+            'tipo' => 'required|max:250',
         ], [
             'nombre.required' => 'El campo nombre es obligatorio.',
             'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
+            'tipo.required' => 'El campo tipo es obligatorio.',
+            'tipo.max' => 'El campo tipo excede el límite de caracteres permitidos.',
             'comentarios.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
-        ] );
+        ]);
         $record = $request->all();
 
-        marca::create( $record );
-        Session::flash( 'message', 1 );
+        marca::create($record);
+        Session::flash('message', 1);
 
-        return redirect()->route( 'catalogoMarca.index' );
+        return redirect()->route('catalogoMarca.index');
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function show( $id ) {
+    public function show($id)
+    {
         //
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function edit( $id ) {
+    public function edit($id)
+    {
         //
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function update( Request $request, $id ) {
+    public function update(Request $request, $id)
+    {
 
-        abort_if ( Gate::denies( 'catalogos_edit' ), 403 );
+        abort_if(Gate::denies('catalogos_edit'), 403);
 
         // dd( $request );
 
-        $request->validate( [
+        $request->validate([
             'nombre' => 'required|max:250',
             'comentarios' => 'nullable|max:500',
         ], [
             'nombre.required' => 'El campo nombre es obligatorio.',
             'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
             'comentarios.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
-        ] );
+        ]);
         $data = $request->all();
 
-        $record = marca::where( 'id', $data[ 'controlId' ] )->first();
+        $record = marca::where('id', $data['controlId'])->first();
 
-        if ( is_null( $record ) == false ) {
+        if (is_null($record) == false) {
             // dd( $data );
-            $record->update( $data );
-            Session::flash( 'message', 1 );
+            $record->update($data);
+            Session::flash('message', 1);
         }
 
-        return redirect()->route( 'catalogoMarca.index' );
+        return redirect()->route('catalogoMarca.index');
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function destroy( $id ) {
+    public function destroy($id)
+    {
         //
     }
 }
