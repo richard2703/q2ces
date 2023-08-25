3a1bb3ee7623fb3b564656f15039acbcc1e1e355
Ricardo Rios <rrios@suappi.com.mx>
2023-08-25 11:38:18 -0600

    correciones en q2ces

--- app/Http/Controllers/catalogosController.php
+++ app/Http/Controllers/catalogosController.php
@@ -25,207 +25,229 @@ use App\Models\maquinariaCategoria;
 use App\Models\maquinariaTipo;
 use App\Models\tipoEquipo;
 
-class catalogosController extends Controller {
+class catalogosController extends Controller
+{
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function index() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
-        return view( 'catalogos.dashCatalogos' );
+    public function index()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
+        return view('catalogos.dashCatalogos');
     }
 
-    public function indexPuestos() {
-        abort_if ( Gate::denies( 'puesto_index' ), 403 );
+    public function indexPuestos()
+    {
+        abort_if(Gate::denies('puesto_index'), 403);
 
-        $records = puesto::select( 'puesto.*', 'puestoNivel.nombre as puestoNivel' )
-        ->leftJoin( 'puestoNivel', 'puestoNivel.id', '=', 'puesto.puestoNivelId' )
-        ->orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = puesto::select('puesto.*', 'puestoNivel.nombre as puestoNivel')
+            ->leftJoin('puestoNivel', 'puestoNivel.id', '=', 'puesto.puestoNivelId')
+            ->orderBy('nombre', 'asc')->paginate(10);
 
-        $vctNiveles = puestoNivel::orderBy( 'nombre', 'asc' )->get();
+        $vctNiveles = puestoNivel::orderBy('nombre', 'asc')->get();
         // dd( $puestos );
-        return view( 'catalogos.puestos', compact( 'records', 'vctNiveles' ) );
+        return view('catalogos.puestos', compact('records', 'vctNiveles'));
     }
 
-    public function indexPuestosNivel() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexPuestosNivel()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = puestoNivel::orderBy( 'nombre', 'asc' )->paginate( 10 );
-        return view( 'catalogos.puestosNivel', compact( 'records' ) );
+        $records = puestoNivel::orderBy('nombre', 'asc')->paginate(10);
+        return view('catalogos.puestosNivel', compact('records'));
     }
 
-    public function indexCatalogoCategoriasTareas() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoCategoriasTareas()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = tareaCategoria::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = tareaCategoria::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.tareaCategorias', compact( 'records' ) );
+        return view('catalogos.tareaCategorias', compact('records'));
     }
 
-    public function indexCatalogoCategoriasMaquinaria() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoCategoriasMaquinaria()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = maquinariaCategoria::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = maquinariaCategoria::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.maquinariaCategorias', compact( 'records' ) );
+        return view('catalogos.maquinariaCategorias', compact('records'));
     }
 
-    public function indexCatalogoTiposMaquinaria() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoTiposMaquinaria()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = maquinariaTipo::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = maquinariaTipo::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.maquinariaTipos', compact( 'records' ) );
+        return view('catalogos.maquinariaTipos', compact('records'));
     }
 
-    public function indexCatalogoTiposTareas() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoTiposTareas()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = tareaTipo::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = tareaTipo::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.tareaTipos', compact( 'records' ) );
+        return view('catalogos.tareaTipos', compact('records'));
     }
 
-    public function indexCatalogoUbicacionesTareas() {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoUbicacionesTareas()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = tareaUbicacion::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = tareaUbicacion::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.tareaUbicaciones', compact( 'records' ) );
+        return view('catalogos.tareaUbicaciones', compact('records'));
     }
 
-    public function indexCatalogoTipoUniforme () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoTipoUniforme()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = tipoUniforme::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = tipoUniforme::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.uniformeTipos', compact( 'records' ) );
+        return view('catalogos.uniformeTipos', compact('records'));
     }
 
-    public function indexCatalogoTiposEquipo () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoTiposEquipo()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = tipoEquipo::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = tipoEquipo::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.equiposTipos', compact( 'records' ) );
+        return view('catalogos.equiposTipos', compact('records'));
     }
 
-    public function indexCatalogoTipoRefaccion () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoTipoRefaccion()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = refaccionTipo::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = refaccionTipo::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.RefaccionTipos', compact( 'records' ) );
+        return view('catalogos.refaccionTipos', compact('records'));
     }
 
-    public function indexCatalogoRefacciones () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoRefacciones()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = refacciones::select( 'refacciones.*', 'marca.nombre as marca', 'maquinaria.nombre as maquinaria', 'refaccionTipo.nombre as tipo' )
-        ->leftJoin( 'marca', 'marca.id', '=', 'refacciones.marcaId' )
-        ->leftJoin( 'maquinaria', 'maquinaria.id', '=', 'refacciones.maquinariaId' )
-        ->leftJoin( 'refaccionTipo', 'refaccionTipo.id', '=', 'refacciones.tipoRefaccionId' )
-        ->orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = refacciones::select('refacciones.*', 'marca.nombre as marca', 'maquinaria.nombre as maquinaria', 'refaccionTipo.nombre as tipo')
+            ->leftJoin('marca', 'marca.id', '=', 'refacciones.marcaId')
+            ->leftJoin('maquinaria', 'maquinaria.id', '=', 'refacciones.maquinariaId')
+            ->leftJoin('refaccionTipo', 'refaccionTipo.id', '=', 'refacciones.tipoRefaccionId')
+            ->orderBy('nombre', 'asc')->paginate(10);
 
-        $vctMarcas = marca::orderBy( 'nombre', 'asc' )->get();
-        $vctMaquinaria = maquinaria::orderBy( 'nombre', 'asc' )->get();
-        $vctTipos = refaccionTipo::orderBy( 'nombre', 'asc' )->get();
+        $vctMarcas = marca::orderBy('nombre', 'asc')->get();
+        $vctMaquinaria = maquinaria::orderBy('nombre', 'asc')->get();
+        $vctTipos = refaccionTipo::orderBy('nombre', 'asc')->get();
         // dd( $puestos );
-        return view( 'catalogos.Refacciones', compact( 'records','vctMarcas','vctMaquinaria','vctTipos' ) );
+        return view('catalogos.refacciones', compact('records', 'vctMarcas', 'vctMaquinaria', 'vctTipos'));
     }
 
-    public function indexCatalogoMarca () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoMarca()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = marca::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = marca::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.marcas', compact( 'records' ) );
+        return view('catalogos.marcas', compact('records'));
     }
 
-    public function indexCatalogoProveedorCategoria () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoProveedorCategoria()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = proveedorCategoria::orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = proveedorCategoria::orderBy('nombre', 'asc')->paginate(10);
         // dd( $puestos );
-        return view( 'catalogos.proveedorCategoria', compact( 'records' ) );
+        return view('catalogos.proveedorCategoria', compact('records'));
     }
 
-    public function indexCatalogoProveedor () {
-        abort_if ( Gate::denies( 'catalogos_index' ), 403 );
+    public function indexCatalogoProveedor()
+    {
+        abort_if(Gate::denies('catalogos_index'), 403);
 
-        $records = proveedor::select( 'proveedor.*', 'proveedorCategoria.nombre as categoria' )
-        ->leftJoin( 'proveedorCategoria', 'proveedorCategoria.id', '=', 'proveedor.categoriaId' )
-        ->orderBy( 'nombre', 'asc' )->paginate( 10 );
-        $vctCategorias = proveedorCategoria::orderBy( 'nombre', 'asc' )->get();
+        $records = proveedor::select('proveedor.*', 'proveedorCategoria.nombre as categoria')
+            ->leftJoin('proveedorCategoria', 'proveedorCategoria.id', '=', 'proveedor.categoriaId')
+            ->orderBy('nombre', 'asc')->paginate(10);
+        $vctCategorias = proveedorCategoria::orderBy('nombre', 'asc')->get();
         // dd( $puestos );
-        return view( 'catalogos.proveedores', compact( 'records', 'vctCategorias' ) );
+        return view('catalogos.proveedores', compact('records', 'vctCategorias'));
     }
 
     /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function create() {
+    public function create()
+    {
         //
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
 
-    public function store( Request $request ) {
+    public function store(Request $request)
+    {
         //
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function show( $id ) {
+    public function show($id)
+    {
         //
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function edit( $id ) {
+    public function edit($id)
+    {
         //
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function update( Request $request, $id ) {
+    public function update(Request $request, $id)
+    {
         //
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function destroy( $id ) {
+    public function destroy($id)
+    {
         //
     }
 }
--- app/Http/Controllers/residenteController.php
+++ app/Http/Controllers/residenteController.php
@@ -12,128 +12,138 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
-class residenteController extends Controller {
+class residenteController extends Controller
+{
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function index() {
-        abort_if ( Gate::denies( 'puesto_index' ), 403 );
+    public function index()
+    {
+        abort_if(Gate::denies('puesto_index'), 403);
 
-        $records = residente::select( 'residente.*',
-        DB::raw( 'obras.nombre AS obra' ) )
-        ->join( 'obras', 'obras.id', '=', 'residente.obraId' )
-        ->orderBy( 'nombre', 'asc' )->paginate( 10 );
+        $records = residente::select(
+            'residente.*',
+            DB::raw('obras.nombre AS obra')
+        )
+            ->join('obras', 'obras.id', '=', 'residente.obraId')
+            ->orderBy('nombre', 'asc')->paginate(10);
 
-        $vctObras = obras::orderBy( 'nombre', 'asc' )->get();
+        $vctObras = obras::orderBy('nombre', 'asc')->get();
         // dd( $puestos );
-        return view( 'mtq.residentes', compact( 'records', 'vctObras' ) );
+        return view('MTQ.residentes', compact('records', 'vctObras'));
     }
 
     /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function create() {
+    public function create()
+    {
         //
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
 
-    public function store( Request $request ) {
-        abort_if ( Gate::denies( 'catalogos_create' ), 403 );
+    public function store(Request $request)
+    {
+        abort_if(Gate::denies('catalogos_create'), 403);
 
         // dd( $request );
-        $request->validate( [
+        $request->validate([
             'nombre' => 'required|max:250',
             'comentario' => 'nullable|max:500',
         ], [
             'nombre.required' => 'El campo nombre es obligatorio.',
             'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
             'comentario.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
-        ] );
+        ]);
         $record = $request->all();
 
-        residente::create( $record );
-        Session::flash( 'message', 1 );
+        residente::create($record);
+        Session::flash('message', 1);
 
-        return redirect()->route( 'residentes.index' );
+        return redirect()->route('residentes.index');
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function show( $id ) {
+    public function show($id)
+    {
         //
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function edit( $id ) {
+    public function edit($id)
+    {
         //
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function update( Request $request, $id ) {
+    public function update(Request $request, $id)
+    {
 
-        abort_if ( Gate::denies( 'catalogos_edit' ), 403 );
+        abort_if(Gate::denies('catalogos_edit'), 403);
 
         // dd( $request );
 
-        $request->validate( [
+        $request->validate([
             'nombre' => 'required|max:250',
             'comentario' => 'nullable|max:500',
         ], [
             'nombre.required' => 'El campo nombre es obligatorio.',
             'nombre.max' => 'El campo título excede el límite de caracteres permitidos.',
             'comentario.max' => 'El campo comentarios excede el límite de caracteres permitidos.',
-        ] );
+        ]);
         $data = $request->all();
 
-        $record = residente::where( 'id', $data[ 'controlId' ] )->first();
+        $record = residente::where('id', $data['controlId'])->first();
 
-        if ( is_null( $record ) == false ) {
+        if (is_null($record) == false) {
             // dd( $data );
-            $record->update( $data );
-            Session::flash( 'message', 1 );
+            $record->update($data);
+            Session::flash('message', 1);
         }
 
-        return redirect()->route( 'residentes.index' );
+        return redirect()->route('residentes.index');
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
-    public function destroy( $id ) {
+    public function destroy($id)
+    {
         //
     }
 }
