6928e05638f591c06e8017676cfd341258b4f80e
KBJ921 <pythondev911@gmail.com>
2023-10-11 11:48:35 -0600

    ajuste

--- resources/views/MTQ/residentes.blade.php
+++ resources/views/MTQ/residentes.blade.php
@@ -87,22 +87,17 @@
                                                             </a>
                                                         @endcan
                                                         @can('residente_mtq_destroy')
-                                                        <form action="{{ route('residentes.destroy', $item->id) }}"
-                                                            method="POST" style="display: inline-block;"
-                                                            onsubmit="return confirm('Seguro?')">
+                                                        <form action="{{ route('residentes.destroy', $item->id) }}" method="POST" style="display: inline-block">
                                                             @csrf
                                                             @method('DELETE')
-                                                            <button class="btnSinFondo" type="submit" rel="tooltip">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="28"
-                                                                    height="28" fill="currentColor"
-                                                                    class="bi bi-x-circle" viewBox="0 0 16 16">
-                                                                    <path
-                                                                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                                    <path
-                                                                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                                            <button class="btnSinFondo sweet-alert-trigger" type="button" rel="tooltip">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                                                                 </svg>
                                                             </button>
                                                         </form>
+                                                        
                                                         @endcan
                                                     </td>
                                                 </tr>
@@ -365,4 +360,31 @@
             const lstObre = document.getElementById('obraId').value = obraId;
         }
     </script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var buttons = document.querySelectorAll('.sweet-alert-trigger');
+    
+            buttons.forEach(function(button) {
+                button.addEventListener('click', function() {
+                    Swal.fire({
+                        title: '¿Seguro?',
+                        text: 'Desea Borrar el Registro',
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Eliminar',
+                        cancelButtonText: 'Cancelar'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // Envía el formulario cuando se confirma
+                            this.closest('form').submit();
+                        }
+                    });
+                });
+            });
+        });
+    </script>
+    
 @endsection
